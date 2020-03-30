6fdf81b5fa83849ed3add1cc228ca3b8fba500b3
bruno <brunooravera@gmail.com>
2020-03-30 16:38:18 -0300

    se agrega producto y compania y se configura api platflor resources, seralization y doctrine orm

---
+++ src/Entity/Company.php
@@ -0,0 +1,90 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\Collection;
+use Ramsey\Uuid\Uuid;
+
+class Company
+{
+    private ?string $id;
+
+    private string $name;
+
+    private \DateTime $createdAt;
+
+    private \DateTime $updatedAt;
+
+    /** @var Collection|Product[] */
+    private Collection $products;
+
+    public function __construct(string $name, string $id = null)
+    {
+        $this->id = $id ?? Uuid::uuid4()->toString();
+        $this->name = $name;
+        $this->createdAt = new \DateTime();
+    }
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get the value of name
+     */ 
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @return  self
+     */ 
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of createdAt
+     */ 
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Get the value of updatedAt
+     */ 
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    public function markAsUpdated(): void
+    {
+        $this->updatedAt = new \DateTime();
+    }
+
+    public function getProducts(): Collection
+    {
+        return $this->products;
+    }
+
+    public function addProduct(Product $product): void
+    {
+        $this->products->add($product);
+
+        $product->addCompany($this);
+    }
+}
---
+++ src/Entity/Product.php
@@ -0,0 +1,104 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Ramsey\Uuid\Uuid;
+
+class Product
+{
+    private ?string $id;
+
+    private string $name;
+
+    private int $price;
+
+    private \DateTime $createdAt;
+
+    private \DateTime $updatedAt;
+
+    /** @var Collection|Company[] */
+    private Collection $companies;
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get the value of name
+     */ 
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @return  self
+     */ 
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of price
+     */ 
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * Set the value of price
+     *
+     * @return  self
+     */ 
+    public function setPrice($price)
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of createdAt
+     */ 
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Get the value of updatedAt
+     */ 
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    public function markAsUpdated(): void
+    {
+        $this->updatedAt = new \DateTime();
+    }
+
+    public function getCompanies()
+    {
+        return $this->companies;
+    }
+
+    public function addCompany(Company $company)
+    {
+        $this->companies->add($company);
+    }
+}
